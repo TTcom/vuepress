adf4dc279e12bf7e08ca401d33fd4f13c5e582e2
ttcom <1902305073@qq.com>
2020-03-30 23:29:16 +0800

    abstractfile

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,31 +1,16 @@
+const nav = require('./nav.js')
+const note = require('./note.js')
+
 module.exports = {
-	// base:"/chat/",
+  // base:"/chat/",
   title: 'We Are Dogs',
   description: 'Just playing around',
-  port:8082,
+  port: 8082,
   head: [
     ['link', { rel: 'icon', href: 'alien.ico' }],
   ],
   themeConfig: {
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'study', link: '/note/' },
-      { text: 'vue', link: '/vue/' },
-      { text: '数组练习', link: '/array/' },
-	    { text: 'javascript', link: '/javascript/' },
-      {
-        text: 'ThinkPHP', items: [
-          { text: 'ThinkPHP5.0', link: '/thinkphp/thinkphp5.0/' },
-        ]
-      },
-      {
-        text: 'webpack', items: [
-          { text: 'webpack3.0', link: '/webpack/webpack3/' },
-          { text: 'webpack4.0', link: '/webpack/webpack4/' }
-        ]
-      },
-      { text: 'github', link: 'https://github.com' },
-    ],
+    nav: nav,
     sidebar: {
       '/array/': [
         {
@@ -44,45 +29,20 @@ module.exports = {
         ['vue3', 'Vue3新特性'],
         ['vuebuttonauth', 'Vue中按钮权限设置']
       ],
-	  '/javascript/': [
-	    {
-	      title: 'javascript',
-	      collapsable: false,
-	    },
-	    ['', 'javascript'],  
-	    ['call_apply_bind', 'call_apply_bind'],  
-		['promise', 'promise'], 
-		['prototype', '原型链'], 
-    ['algorithm', '算法'],
-    ['regex', '正则'],
-    ['eventloop', 'eventloop'],
-	  ],
-      '/note/': [
+      '/javascript/': [
         {
-          title: 'study',
+          title: 'javascript',
           collapsable: false,
         },
-        ['', 'chrome检测内存泄漏'],   //第一个元素为默认路径，第二个元素为设置的别名
-        ['nodeDeploy', 'node项目部署'],
-        ['pspeople', '快速抠出头像'],
-		['vueheight', 'vueheight'],
-		['layout', '移动端适配问题'],
-		['aboutgit', '关于git'],
-		['aboutcss', 'aboutcss'],
-		['rowcss', 'rowcss'],
-		['vueplugin', 'vueplugin'],
-		['vue_proxy', 'vue_proxy'],
-		['vue_jwt', 'vue_jwt'],
-		['treefind', '遍历树结构数据'],
-		['npm_releasevue', 'npm_releasevue'],
-		['classArray', '类数组对象'],
-		['deepclone', '深拷贝'],
-		['center', '垂直水平居中'], 
-		['svg', 'svg'],
-		['learndocker', 'learndocker'],
-		['moregitssh', '配置多个ssh'],
-		['funcomponent', 'vue函数式组件']
+        ['', 'javascript'],
+        ['call_apply_bind', 'call_apply_bind'],
+        ['promise', 'promise'],
+        ['prototype', '原型链'],
+        ['algorithm', '算法'],
+        ['regex', '正则'],
+        ['eventloop', 'eventloop'],
       ],
+      '/note/': note,
       '/thinkphp/thinkphp5.0/': [
         {
           title: 'thinkphp5.0',
@@ -109,12 +69,9 @@ module.exports = {
         },
         ['', '前言'],   //第一个元素为默认路径，第二个元素为设置的别名
         'module',
-		    ['aboutvue', 'aboutvue']
+        ['aboutvue', 'aboutvue']
 
       ],
-
-
-
 
     },
     sidebarDepth: 23
---
+++ docs/.vuepress/nav.js
@@ -0,0 +1,19 @@
+module.exports = [
+    { text: 'Home', link: '/' },
+    { text: 'study', link: '/note/' },
+    { text: 'vue', link: '/vue/' },
+    { text: '数组练习', link: '/array/' },
+    { text: 'javascript', link: '/javascript/' },
+    {
+      text: 'ThinkPHP', items: [
+        { text: 'ThinkPHP5.0', link: '/thinkphp/thinkphp5.0/' },
+      ]
+    },
+    {
+      text: 'webpack', items: [
+        { text: 'webpack3.0', link: '/webpack/webpack3/' },
+        { text: 'webpack4.0', link: '/webpack/webpack4/' }
+      ]
+    },
+    { text: 'github', link: 'https://github.com' },
+  ]
