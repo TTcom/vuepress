84f211b5bddad6cd5e6f5a9731d91f1d7bcd1fa6
Your Name <1902305073@qq.com>
2020-08-06 11:00:47 +0800

    保留必要的目录结构，方便从零搭建

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,4 @@
 const nav = require('./nav.js')
-const note = require('./note.js')
 
 module.exports = {
   //base:"./",
@@ -12,70 +11,17 @@ module.exports = {
   themeConfig: {
     nav: nav,
     sidebar: {
-      '/array/': [
-        {
-          title: '数组练习',
-          collapsable: false,
-        },
-        ['', '获取数组中出现次数最多的字符'],
-        ['flat', 'ES10flat'],
-      ],
-      '/vue/': [
-        {
-          title: 'VUE',
-          collapsable: false,
-        },
-        ['', 'Vue打包文件浏览器存在缓存问题'],
-        ['vue3', 'Vue3新特性'],
-        ['vuebuttonauth', 'Vue中按钮权限设置'],
-        ['vuex', 'vuex'],
-        ['vueantic', 'vueantic'],
-        ['vue_eslint', 'vue_eslint'],
-        ['vue_template_invscode', 'vue_template_invscode'],
-        ['vueplugin', 'vueplugin'],
-        ['vue_proxy', 'vue_proxy'],
-        ['vue_jwt', 'vue_jwt'],
-        ['vue_transition', 'vue_transition'],
-        ['funcomponent', 'vue函数式组件']
-      ],
       '/javascript/': [
         {
           title: 'javascript',
           collapsable: false,
         },
         ['', 'javascript'],
-        ['call_apply_bind', 'call_apply_bind'],
-        ['promise', 'promise'],
-        ['prototype', '原型链'],
-        ['algorithm', '算法'],
-        ['regex', '正则'],
-        ['eventloop', 'eventloop'],
-      ],
-      '/note/': note,
-      '/webpack/': [
-        {
-          title: 'WebPack',
-          collapsable: false,
-        },
-        ['', 'WebPack'],   //第一个元素为默认路径，第二个元素为设置的别名
-      ],
-      '/nginx/': [
-        {
-          title: 'Nginx',
-          collapsable: false,
-        },
-        ['', 'nginx'],   //第一个元素为默认路径，第二个元素为设置的别名
+        ['algorithm', '算法']
       ],
 
     },
     sidebarDepth: 23
-
-
-
   },
-
-
-
-
 
 }
--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,10 +1,5 @@
 module.exports = [
     { text: 'Home', link: '/' },
-    { text: 'study', link: '/note/' },
-    { text: 'vue', link: '/vue/' },
-    { text: '数组练习', link: '/array/' },
     { text: 'javascript', link: '/javascript/' },
-    { text: 'webpack', link: '/webpack/' },
-    { text: 'Nginx', link: '/nginx/' },
-    { text: 'github', link: 'https://github.com' },
+    { text: 'github', link: 'https://github.com/TTcom/vuepress' },
   ]
